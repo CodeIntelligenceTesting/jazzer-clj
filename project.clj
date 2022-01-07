aa24039e410d17b68dc99e39d71e543af99a2748
Sebastian Poeplau <poeplau@code-intelligence.com>
2022-01-07 15:22:57 +0100

    Fix initial REPL namespace

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.code-intelligence/jazzer-api "0.10.0"]]
-  :repl-options {:init-ns jazzer-clj.core})
+  :repl-options {:init-ns com.code-intelligence.jazzer-clj.core})
